e28600ca8dd2e6587086671b4e973690595aece9
vVikingNL <34035063+Datacenter-Dennisch@users.noreply.github.com>
2020-09-07 13:51:15 +0200

    Update Add-NSXFirewallRuleService.ps1

--- Add-NSXFirewallRuleService.ps1
+++ Add-NSXFirewallRuleService.ps1
@@ -1,3 +1,4 @@
+
 function Add-NsxFirewallRuleService {
 
     <#
@@ -28,6 +29,9 @@ function Add-NsxFirewallRuleService {
         [Parameter (Mandatory=$true)]
             [ValidateNotNullOrEmpty()]
             [System.Xml.XmlElement]$Service,
+        [Parameter (Mandatory=$False)]
+            # Prompt for confirmation.  Specify as -confirm:$false to disable confirmation prompt
+            [switch]$Confirm=$true,
         [Parameter (Mandatory=$false)]
             #PowerNSX Connection object.
             [ValidateNotNullOrEmpty()]
@@ -37,6 +41,17 @@ function Add-NsxFirewallRuleService {
     begin {}
 
     process {
+        if ( $confirm ) {
+            $message  = "Adding NSX Service object $($Service.name) to NSX Distribute Firewall rule $($FirewallRule.name)."
+            $question = "Proceed with adding service object to NSX DFW rule?"
+            $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+            $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+            $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+            $decision = $Host.UI.PromptForChoice($message, $question, $choices, 1)
+        }
+        else { $decision = 0 }
+
         $sectionId = $FirewallRule.ParentNode.Id
         $RuleId = $FirewallRule.id
         $generationNumber = $FirewallRule.ParentNode.generationnumber
@@ -52,14 +67,15 @@ function Add-NsxFirewallRuleService {
         
         $uri = "/api/4.0/firewall/globalroot-0/config/layer3sections/$sectionId/rules/$Ruleid"
         $IfMatchHeader = @{"If-Match"=$generationNumber}
-
-        try {
-            $response = Invoke-NsxWebRequest -method put -Uri $uri -body $xmldoc.rule.OuterXml -extraheader $IfMatchHeader -connection $connection
-            [xml]$ruleElem = $response.Content
-            Get-NsxFirewallRule -RuleId $ruleElem.rule.id
-        }
-        catch {
-            throw "Failed to modify the specified rule.  $_"
+        if ($decision -eq 0) {
+            try {
+                $response = Invoke-NsxWebRequest -method put -Uri $uri -body $xmldoc.rule.OuterXml -extraheader $IfMatchHeader -connection $connection
+                [xml]$ruleElem = $response.Content
+                Get-NsxFirewallRule -RuleId $ruleElem.rule.id
+            }
+            catch {
+                throw "Failed to modify the specified rule.  $_"
+            }
         }
     }
 
