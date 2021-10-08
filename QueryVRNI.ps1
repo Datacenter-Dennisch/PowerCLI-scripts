0a36b5d75b44562cba9f1d72c5599286d2498c4b
vVikingNL <34035063+Datacenter-Dennisch@users.noreply.github.com>
2021-10-08 17:22:14 +0200

    Update QueryVRNI.ps1

--- QueryVRNI.ps1
+++ QueryVRNI.ps1
@@ -23,16 +23,20 @@ $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
 [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 
 $site = "https://vrni.domain.local"
-$vrnicred = Get-Credential -Message "Enter admin credentials $site"
-$username = $vrnicred.UserName
-$password = $vrnicred.GetNetworkCredential().Password
-$domaintype = "LDAP"
-$domain = "ssc.lan"
+if (!$apiKey) {
+    do {
+        $vrnicred = Get-Credential -Message "Enter admin credentials $site"
+        $username = $vrnicred.UserName
+        $password = $vrnicred.GetNetworkCredential().Password
+        $domaintype = "LDAP"
+        $domain = "domain.local"
 
-$jsonCredentials = @{username=$username;password=$password;domain=@{domain_type=$domaintype;value="$domain"}} | ConvertTo-Json
+        $jsonCredentials = @{username=$username;password=$password;domain=@{domain_type=$domaintype;value="$domain"}} | ConvertTo-Json
 
-$url = $site +"/api/ni/auth/token"
-$apiKey = Invoke-RestMethod $url -Method POST -Body $jsonCredentials -ContentType 'application/json'
+        $url = $site +"/api/ni/auth/token"
+        $apiKey = Invoke-RestMethod $url -Method POST -Body $jsonCredentials -ContentType 'application/json'
+    } until  ($apiKey)
+}
 $authVal = "NetworkInsight " + $apiKey.token
 
 $VM = (get-vm | out-gridview -Title "Select VM to analyze" -PassThru).name
@@ -45,47 +49,53 @@ $entityid = $returnjson.results.entity_id
 #get flows from source VM
 
 $flowids = @()
-$jsonbody = @{entity_type="flow";size="10"; filter="source_vm.entity_id = '$entityid'"} | ConvertTo-Json #;time_range= @{start_time=[int]$epochdaysofdata;end_time=[int]$epochnow}}
+$jsonbody = @{entity_type="flow";size="100"; filter="source_vm.entity_id = '$entityid'"} | ConvertTo-Json #;time_range= @{start_time=[int]$epochdaysofdata;end_time=[int]$epochnow}}
 $url = $site +"/api/ni/search"
 $returnjson = Invoke-RestMethod $url -Method POST -ContentType 'application/json' -Headers @{"AUTHORIZATION"=$authVal} -Body $jsonbody
 $flowids += $returnjson.results.entity_id 
 
 #get flows from destination VM
-$jsonbody = @{entity_type="flow";size="10"; filter="destination_vm.entity_id = '$entityid'"} | ConvertTo-Json #;time_range= @{start_time=[int]$epochdaysofdata;end_time=[int]$epochnow}}
+$jsonbody = @{entity_type="flow";size="100"; filter="destination_vm.entity_id = '$entityid'"} | ConvertTo-Json #;time_range= @{start_time=[int]$epochdaysofdata;end_time=[int]$epochnow}}
 $url = $site +"/api/ni/search"
 $returnjson = Invoke-RestMethod $url -Method POST -ContentType 'application/json' -Headers @{"AUTHORIZATION"=$authVal} -Body $jsonbody
 $flowids += $returnjson.results.entity_id
 
-
-$objDetails = foreach ($flowid in $flowids){
+$return = @()
+foreach ($flowid in $flowids){
     $url = $site +"/api/ni/entities/flows/$flowid"
     $returnjson = Invoke-RestMethod $url -Method GET -ContentType 'application/json' -Headers @{"AUTHORIZATION"=$authVal}
     
     $url = $site +"/api/ni/entities/vms/$($returnjson.source_vm.entity_id)"
     $SourceVM = Invoke-RestMethod $url -Method GET -ContentType 'application/json' -Headers @{"AUTHORIZATION"=$authVal}
     
-    $SourceSG = foreach ($Sg in $returnjson.source_security_groups.entity_id) {
-        $url = $site +"/api/ni/entities/security-groups/$Sg"
-        Invoke-RestMethod $url -Method GET -ContentType 'application/json' -Headers @{"AUTHORIZATION"=$authVal}
-    }
+    $SGbody = $SourceVM.security_groups 
+    if ($SGbody) {
+        $jsonbody = @{entity_ids=$SGbody} | ConvertTo-Json
+        $url = $site +"/api/ni/entities/fetch"
+        $sourceSGName = (Invoke-RestMethod $url -Method post -ContentType 'application/json' -Headers @{"AUTHORIZATION"=$authVal} -Body $jsonbody).results.entity.name -join ","
+    } else {$sourceSGName = ""}
 
     $url = $site +"/api/ni/entities/vms/$($returnjson.destination_vm.entity_id)"
     $DestinationVM = Invoke-RestMethod $url -Method GET -ContentType 'application/json' -Headers @{"AUTHORIZATION"=$authVal}
-    $DestinationSG = foreach ($Sg in $DestinationVM.security_groups) {
-        $url = $site +"/api/ni/entities/security-groups/$($Sg.entity_id)"
-        #Invoke-RestMethod $url -Method GET -ContentType 'application/json' -Headers @{"AUTHORIZATION"=$authVal}
-    }
-
+    
+    $SGbody = $DestinationVM.security_groups 
+    if ($SGbody) {
+        $jsonbody = @{entity_ids=$SGbody} | ConvertTo-Json
+        $url = $site +"/api/ni/entities/fetch"
+        $DestinationSGName = (Invoke-RestMethod $url -Method post -ContentType 'application/json' -Headers @{"AUTHORIZATION"=$authVal} -Body $jsonbody).results.entity.name -join ","
+    } else {$DestinationSGName = ""}
     $ReturnItem = [pscustomobject][ordered]@{        
-        ruleid = $PsEventItem.vmwnsxfirewallruleid
-        vmwnsxfirewallsrc = $PsEventItem.vmwnsxfirewallsrc
-        sourceVMname = $SourceVM.name -join ","
-        sourceSGname = $SourceSG.name -join ","
-        vmwnsxfirewalldst = $PsEventItem.vmwnsxfirewalldst
-        destinationVMname = $DestinationVM.name -join ","
-        destinationSGName = $DestinationSG.name -join ","
+        #ruleid = $PsEventItem.vmwnsxfirewallruleid
+        vmwnsxfirewallsrc = $returnjson.source_ip.ip_address
+        sourceVMname = $SourceVM.name 
+        sourceSGname = $SourceSG.name
+        vmwnsxfirewalldst = $returnjson.destination_ip.ip_address
+        destinationVMname = $DestinationVM.name 
+        destinationSGName = $DestinationSG.name 
         protocol = $returnjson.protocol
         port = $returnjson.port.display
         action = $returnjson.firewall_action
     }
+    $return += $ReturnItem
 }
+$return | Out-GridView
